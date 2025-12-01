d171c539cb8d19a7253973f41cf6e56c82feb32c
robert01mm <mmasirobert08@gmail.com>
2025-12-01 06:59:44 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return (num1 > num2) ? num1 : num2;
+}
 
+function isPalindrome(word) {
+    let reversed = word.split("").reverse().join("");
+    return word === reversed;
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let discountAmount = (discountPercentage / 100) * originalPrice;
+    return originalPrice - discountAmount;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = {
+    calculateTax,
+    convertToUpperCase,
+    findMaximum,
+    isPalindrome,
+    calculateDiscountedPrice
+};
